89b577b4244f4bdde4c43ca411eed12547828ea8
Sofia <srumbosnieves@gmail.com>
2022-09-28 22:29:46 -0300

    Alteração cadastro

--- src/Pages/Cadastro/cadastro.js
+++ src/Pages/Cadastro/cadastro.js
@@ -1,54 +1,68 @@
 import React from 'react';
 import './cadastro.css'
+import undraw from '../../img/undraw.png';
 
 export function Cadastro() {
-    return (
-       <div className='Container_cadastro'>
-        <form className="form" >
+  return (
+    <body>
+      <div className='container_formulário'>
+        
+        <div className='form-img'>
+          <img src={undraw} alt='Cadastro' />
+        </div>
+
+          <form className="form" >
             <legend className="legend"><b>Criar Conta</b></legend>
             <p>Já é um membro?<a href='http://localhost:3002/login'>Login</a></p>
-            <br></br>
+            <br/>
             <div className="inputBox">
-              <input type="text" required="yes" name="nome" id="nome" class="inputUser required"/>
+              <input type="text" required="yes" name="nome" id="nome" class="inputUser required" />
               <label for="nome" className="labelInput">Nome Completo</label>
             </div>
-            <br></br>
+            <br/>
             <div className="inputBox">
-              <input type="text" required="yes" name="email" id="email" class="inputUser required"/>
+              <input type="text" required="yes" name="email" id="email" class="inputUser required" />
               <label for="email" className="labelInput">Email</label>
             </div>
-            <br></br>
+            <br/>
             <div className="inputBox">
-              <input type="tel" required="yes" name="telefone" id="telefone" class="inputUser required"/>
+              <input type="tel" required="yes" name="telefone" id="telefone" class="inputUser required" />
               <label for="telefone" className="labelInput">Telefone</label>
             </div>
-            <br></br>
+            <br/>
+            <div className='input-genero'>
             <p>Sexo:</p>
-            <input type="radio" id="feminino" name="genero" value="feminino" required/>
+            <input type="radio" id="feminino" name="genero" value="feminino" required />
             <label for="feminino">Feminino</label>
-            <input type="radio" id="masculino" name="genero" value="masculino" required/>
+            <input type="radio" id="masculino" name="genero" value="masculino" required />
             <label for="masculino">Masculino</label>
-            <input type="radio" id="outro" name="genero" value="outro" required/>
+            <input type="radio" id="outro" name="genero" value="outro" required />
             <label for="outro">Outro</label>
-            <br></br>
+            </div>
+            <br/>
+            <div  className='data'>
             <label for="data-nascimento"><b>Data de Nascimento:</b></label>
-            <input type="date" name="data-nascimento" id="data-nascimento"/>
-            <br></br>
-            <br></br>
+            <input type="date" name="data-nascimento" id="data-nascimento" />
+            </div>
+            <br/>
             <div className="inputBox">
-              <input required="yes" name="senha" id="senha" class="inputUser required"/>
+              <input required="yes" name="senha" id="senha" class="inputUser required" />
               <label for="sennha" className="labelInput">Senha</label>
             </div>
-            <br></br>
+            <br/>
             <div className="inputBox">
-              <input required="yes" name="confirmar" id="confirmar" class="inputUser required"/>
+              <input required="yes" name="confirmar" id="confirmar" class="inputUser required" />
               <label for="confirmar" className="labelInput">Confirmar Senha</label>
             </div>
-            <br></br>
+            <br/>
+            <input type="file" />
+            <br/>
             <input type="submit" name="submit" id="submit" ></input>
-        </form>
-    </div>
-    );
-}
+          </form>
+     
+      </div>
+    </body>
+  );
+};
 
 export default Cadastro;
