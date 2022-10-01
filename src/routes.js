d59603734cb4c8ea4337e087b5131fdf8d543202
Alves Script <100441454+alvesscript@users.noreply.github.com>
2022-09-30 23:09:55 -0300

    levando a api

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./Pages/Home/Home";
 import { Cadastro } from "./Pages/Cadastro/cadastro";
 import {Login} from "./Pages/Login/login";
+import {Tabelaapi} from "./Pages/api/apicadastro";
 
 function Rotas() {
     return (
@@ -11,6 +12,7 @@ function Rotas() {
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/Cadastro" element={<Cadastro />}></Route>
                 <Route path="/Login" element={<Login />}></Route>
+                <Route path="adm" element={<Tabelaapi />}></Route>
             </Routes>
         </BrowserRouter>
     );
